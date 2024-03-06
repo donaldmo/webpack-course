1455a45b672bdf26433319df1b81057376d6b87c
Donald Motswiri <rmotswiri023@student.wethinkcode.co.za>
2024-03-06 14:45:17 +0200

    working on babel - giving errors

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,48 +1,55 @@
 const path = require("path")
 
 module.exports = {
-    entry: {
-        main: ["./src/main.js"]
-    },
-    mode: "development",
-    output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" }, { loader: "css-loader" }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "file-loader", options: { name: "[name].[ext]" }
-                    },
-                    {
-                        loader: "extract-loader", options: { publicPath: "../" }
-                    },
-                    { loader: "html-loader" }
-                ]
-            },
-            {
-                test: /\.jpg$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: { name: "images/[name].[ext]" }
-                    }
-                ]
-            },
-        ]
-    },
-    devServer: {
-        contentBase: "dist",
-        overlay: true
-    }
+	entry: {
+		main: ["./src/main.js"]
+	},
+	mode: "development",
+	output: {
+		filename: "[name]-bundle.js",
+		path: path.resolve(__dirname, "../dist"),
+		publicPath: "/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{ loader: "babel-loader"}
+				],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: "style-loader" }, { loader: "css-loader" }
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "file-loader", options: { name: "[name].[ext]" }
+					},
+					{
+						loader: "extract-loader", options: { publicPath: "../" }
+					},
+					{ loader: "html-loader" }
+				]
+			},
+			{
+				test: /\.(jpg|png|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: { name: "images/[name]-[hash:8].[ext]" }
+					}
+				]
+			},
+		]
+	},
+	devServer: {
+		contentBase: "dist",
+		overlay: true
+	}
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+require("babel-runtime/regenerator")
 require("./main.css")
+require("./images/link.jpg")
 require("./index.html")
 
-function hello() {
-    console.log('donald')
+var a = async args => {
+  const { a, b } = args
+  await console.log("Hello from the future!", a, b)
 }
+
+a({ a: 1, b: 2 })
